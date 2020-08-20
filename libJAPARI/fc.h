b9e819bbcffb036c381257968bfdf0a8210c807a
A-Kai Kang <akaikang@users.noreply.github.com>
2020-08-20 16:30:09 +0800

    Create fc.h

---
+++ libJAPARI/fc.h
@@ -0,0 +1,12 @@
+#ifndef JAPARILIB_JAP_FC_H_
+#define JAPARILIB_JAP_FC_H_
+#include <msp430.h>
+#include <stdint.h>
+#include "driverlib.h"
+#include "DSPLib.h"
+#include "japari.h"
+
+void JAP_FC(JAP_LAYER* LAYER);
+void HAW_FC(JAP_LAYER* LAYER);
+
+#endif /* JAPARILIB_JAP_FC_H_ */
