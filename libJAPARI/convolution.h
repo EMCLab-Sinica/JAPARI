3726ee294172f131dac8a558df6a107365b67cf7
A-Kai Kang <akaikang@users.noreply.github.com>
2020-08-20 16:28:39 +0800

    Create convolution.h

---
+++ libJAPARI/convolution.h
@@ -0,0 +1,12 @@
+#ifndef JAPARILIB_JAP_IMP_H_
+#define JAPARILIB_JAP_IMP_H_
+#include <msp430.h>
+#include <stdint.h>
+#include "driverlib.h"
+#include "DSPLib.h"
+#include "japari.h"
+
+void JAP_CONV(JAP_LAYER* LAYER);
+void HAW_CONV(JAP_LAYER* LAYER);
+
+#endif /* JAPARILIB_JAP_IMP_H_ */
