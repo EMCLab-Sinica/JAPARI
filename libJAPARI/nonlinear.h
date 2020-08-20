1fcd32335bc5f8749229c601e63c3a4e9ab82227
A-Kai Kang <akaikang@users.noreply.github.com>
2020-08-20 16:33:24 +0800

    Create nonlinear.h

---
+++ libJAPARI/nonlinear.h
@@ -0,0 +1,12 @@
+#ifndef JAPARILIB_JAP_NONLINEAR_H_
+#define JAPARILIB_JAP_NONLINEAR_H_
+#include <msp430.h>
+#include <stdint.h>
+#include "driverlib.h"
+#include "DSPLib.h"
+#include "japari.h"
+
+void JAP_POOL(JAP_LAYER* LAYER);
+void HAW_POOL(JAP_LAYER* LAYER);
+
+#endif /* JAPARILIB_JAP_NONLINEAR_H_ */
